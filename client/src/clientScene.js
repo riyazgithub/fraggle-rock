0b4d80a6d2cdb91d7f4a83e2cb74555bc79ff71e
Eric Eakin <eseakin@gmail.com>
2016-11-10 14:44:35 -0800

    Added textures to clientScene, fixed minor typo in webpack, added model.json for testing adding a single mesh.

--- client/src/clientScene.js
+++ client/src/clientScene.js
@@ -22,6 +22,69 @@ const startUpdateTick = function startUpdateTick(camera) {
   }, serverUpdateInterval);
 };
 
+  // load a texture, set wrap mode to repeat
+  var texture = new THREE.TextureLoader().load( "textures/woodcrate.jpg" );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 1 );
+
+  var grass = new THREE.TextureLoader().load( "textures/grass-repeating.jpg" );
+  grass.wrapS = THREE.RepeatWrapping;
+  grass.wrapT = THREE.RepeatWrapping;
+  grass.repeat.set( 40, 40 );
+
+  //loader test
+  var mesh1 = null;
+  var loader = new THREE.BufferGeometryLoader();
+  loader.load('../../model.json', function(geometry) {
+    var material = new THREE.MeshLambertMaterial( { color: 0xF5F5F5, map: texture } );
+    mesh1 = new THREE.Mesh(geometry, material);
+    console.log('loaded', mesh1)
+    scene.add(mesh1);
+
+  }, function() {
+    console.log('what does this do?')
+  });
+
+  // Ambient Light
+  scene.add(new THREE.AmbientLight(0x111111));
+
+  // Green Square
+  let geometry = new THREE.BoxGeometry(1, 1, 1);
+  let material = new THREE.MeshBasicMaterial({ color: 'green', map: texture });
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 5;
+  mesh.position.y = .5;
+  scene.add(mesh);
+
+  // Red Square
+  material = new THREE.MeshBasicMaterial({ color: 'red', map: texture });
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = -5;
+  mesh.position.y = .5;
+  scene.add(mesh);
+
+  // Blue Square
+  material = new THREE.MeshBasicMaterial({ color: 'blue', map: texture });
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = 5;
+  mesh.position.y = .5;
+  scene.add(mesh);
+
+  // Yellow Square
+  material = new THREE.MeshBasicMaterial({ color: 'yellow', map: texture });
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = -5;
+  mesh.position.y = .5;
+  scene.add(mesh);
+
+  // FLOOR
+  geometry = new THREE.BoxGeometry(100, 0.1, 100);
+  material = new THREE.MeshBasicMaterial({ color: 'white', map: grass });
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = -.05;
+  scene.add(mesh);
+
 const startGame = function startGame() {
   const game = init(); //creates camera, renderer and scene data
   sceneUtility.addLookControls(game.camera);

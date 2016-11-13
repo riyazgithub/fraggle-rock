699eaee563e5c3b5b516b36083eaa69224dfb3a6
NickLathen <nick.lathen@gmail.com>
2016-11-13 01:08:32 -0800

    Add rounding to server physics emits

--- server/models/match.js
+++ server/models/match.js
@@ -73,14 +73,35 @@ const startPhysics = function startPhysics(io) {
   }, this.physicsTick)
 
 
+
+  const roundToDec = function round(num, decimals) {
+    decimals = decimals || 3;
+    const mult = Math.pow(10, decimals);
+    return Math.round(num * mult) / mult;
+  }
+  const roundPosition = function roundPosition (position, decimals) {
+    const newPosition = {};
+    newPosition.x = roundToDec(position.x, decimals);
+    newPosition.y = roundToDec(position.y, decimals);
+    newPosition.z = roundToDec(position.z, decimals);
+    return newPosition;
+  };
+  const roundQuaternion = function roundQuaternion (quaternion, decimals) {
+    const newQuaternion = {};
+    newQuaternion.w = roundToDec(quaternion.w, decimals);
+    newQuaternion.x = roundToDec(quaternion.x, decimals);
+    newQuaternion.y = roundToDec(quaternion.y, decimals);
+    newQuaternion.z = roundToDec(quaternion.z, decimals);
+    return newQuaternion;
+  };
   this.physicsEmitClock = setInterval(function() {
     const balls = [];
     const boxes = [];
     context.balls.forEach(function(ball) {
-      balls.push({uuid: ball.id, position: ball.position, quaternion: ball.quaternion, mass: ball.mass})
+      balls.push({uuid: ball.id, position: roundPosition(ball.position), quaternion: roundQuaternion(ball.quaternion), mass: ball.mass})
     })
     context.boxes.forEach(function(box) {
-      boxes.push({uuid: box.uuid, position: box.position, quaternion: box.quaternion, geometry: box.userData.geometry, type: box.userData.shapeType, mass: box.mass})
+      boxes.push({uuid: box.uuid, position: roundPosition(box.position), quaternion: roundQuaternion(box.quaternion), geometry: box.userData.geometry, type: box.userData.shapeType, mass: box.mass})
     })
     io.to(context.guid).volatile.emit('physicsUpdate', JSON.stringify({boxMeshes: boxes, ballMeshes: balls, players: context.clients}))
   }, this.physicsEmitTick)

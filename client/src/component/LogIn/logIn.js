cc243b4419dd846a77a17f6664f1659ad1e79059
Will Stockman <willstockman13@Wills-MacBook-Pro.local>
2016-11-15 11:35:34 -0800

    Log In with Facebook working

--- client/src/component/LogIn/logIn.js
+++ client/src/component/LogIn/logIn.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login'
 
+class LogIn extends React.Component {
+  constructor(props) {
+	  super(props);
+	  this.state = {
+	    user: null
+	  };
+  }
+  componentClicked(e) {
+  	console.log(e)
+  }
 
-var LogIn = React.createClass({
-  render: function() {
+  responseFacebook(e) {
+    this.setState({ user: true })
+  }
+
+  render() {
     return (
       <div className='LogIn'>
-        <button> Log In With Facebook</button>
+        <FacebookLogin
+            appId="1709766049351226"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook}
+        />
       </div>
     );
+  	
   }
-});
+}
+
 
 
 export default LogIn;

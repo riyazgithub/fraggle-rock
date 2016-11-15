150965fc07d057c9fab2d76b0ef99ba7375ab378
Will Stockman <willstockman13@Wills-MacBook-Pro.local>
2016-11-15 11:05:52 -0800

    Added basic react structure

---
+++ client/src/component/LogIn/logIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+var LogIn = React.createClass({
+  render: function() {
+    return (
+      <div className='LogIn'>
+        <button> Log In With Facebook</button>
+      </div>
+    );
+  }
+});
+
+
+export default LogIn;
